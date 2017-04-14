2b5bb0f9993a7660054aa70642f882f4cec12b69
PabloPicassoft <pablo.picassoft@gmail.com>
2017-04-14 17:52:29 +0100

    Layouts for Login, Sign Up activities have been created. New Splashscreen logo.

---
+++ app/src/main/java/com/picassoft/mymedicare/CalculateRisk.java
@@ -0,0 +1,13 @@
+package com.picassoft.mymedicare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculateRisk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_risk);
+    }
+}
---
+++ app/src/main/java/com/picassoft/mymedicare/LoginScreen.java
@@ -0,0 +1,19 @@
+package com.picassoft.mymedicare;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
+
+
+
